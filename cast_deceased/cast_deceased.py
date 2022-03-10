705013d953ffba9d0647dcb897bb7f12a0d5a96a
Guillaume Faguet <guillaume.faguet@protonmail.com>
2022-03-10 11:27:53 +0100

    simplified function actor details

--- cast_deceased/cast_deceased.py
+++ cast_deceased/cast_deceased.py
@@ -14,19 +14,19 @@ alive = 0
 
 
 def actor_details(actor):
+    # return death date and avatar for a given actor ID
     actor_info = ia.get_person(actor.personID, info="biography")
+    # check if death date exists
     try:
-        return f':skull_and_crossbones: {actor_info["death date"]}'
+        death_date = actor_info["death date"]
     except:
-        return "Alive"
-
-
-def actor_avatar(actor):
-    actor_info = ia.get_person(actor.personID, info="biography")
+        death_date = "Alive"
+    # check if avatar exists
     try:
-        return actor_info["headshot"]
+        avatar = actor_info["headshot"]
     except:
-        return "ressources/avatar.jpg"
+        avatar = "ressources/avatar.jpg"
+    return [f":skull_and_crossbones: {death_date}", avatar]
 
 
 try:
@@ -47,23 +47,23 @@ else:
         h2.markdown(f'Year: {movie_details["year"]}')
         try:
             for director in movie_details["director"]:
-                h2.markdown(f"Director: {director}, {actor_details(director)}")
+                h2.markdown(f"Director: {director}, {actor_details(director)[0]}")
         except:
             h2.markdown("No director found")
         h2.metric(label="Total actor (limited to 20)", value=len(cast_list))
 
     # MultiThreading. Using map instead submit to keep actor order.
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        avatars = executor.map(actor_avatar, cast_list)
+        results = executor.map(actor_details, cast_list)
 
         list_avatar = []
-        for avatar in avatars:
-            list_avatar.append(avatar)
+        list_death = []
+        for result in results:
+            list_avatar.append(result[1])
+            list_death.append(result[0])
+
         st.image(list_avatar, width=67, caption=cast_list)
 
-    # MultiThreading
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        results = executor.map(actor_details, cast_list)
     cast = st.container()
     with cast:
         col1, col2 = st.columns(2)
@@ -74,9 +74,9 @@ else:
         # actor names
         [col1.markdown(actor) for actor in cast_list]
 
-        for result in results:
-            col2.markdown(result)
-            if result == "Alive":
+        for date in list_death:
+            col2.markdown(date)
+            if date == "Alive":
                 alive += 1
 
     with header:
